5013abb9cb42c388086dc192d6f31e94f6e8a9d6
Badsha-Nishan <badshanisan14@gmail.com>
2026-01-28 23:11:46 +0600

    completed without task 5

--- index.js
+++ index.js
@@ -19,17 +19,29 @@
 // Task-3:
 // Check whether a string contains all the vowels a, e, i, o, u
 
-const contain =
-  "is an Emmet abbreviation used in code editors like Visual Studio Code to instantly generate 20 words of placeholder text. Typing this in HTML and pressing Tab (or Enter in some editors) produces a 20-word string for web design mockups. ";
+// const contain =
+//   "is an Emmet abbreviation used in code editors like Visual Studio Code to instantly generate 20 words of placeholder text. Typing this in HTML and pressing Tab (or Enter in some editors) produces a 20-word string for web design mockups. ";
 
-if (
-  contain.includes("a") &&
-  contain.includes("e") &&
-  contain.includes("i") &&
-  contain.includes("o") &&
-  contain.includes("u")
-) {
-  console.log("All the character is matching");
-} else {
-  console.log("some not matched");
-}
+// if (
+//   contain.includes("a") &&
+//   contain.includes("e") &&
+//   contain.includes("i") &&
+//   contain.includes("o") &&
+//   contain.includes("u")
+// ) {
+//   console.log("All the character is matching");
+// } else {
+//   console.log("some not matched");
+// }
+
+// Task-4:
+// If a given string has either x, replace x by y. if the given string has X, replace it by Y.
+
+// let x = "Badsha Nishan Is very cool man";
+// const y = x.replace("man", "person"); //first value is search item, then replace item.
+// console.log(y);
+
+// Task-5:
+// Capitalize Every first Letter of each word in a String
+
+
