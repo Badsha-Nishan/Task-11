8e4fdf269af48e898c8459ceae7aa6afc6c6c8ed
Badsha-Nishan <badshanisan14@gmail.com>
2026-01-28 20:01:31 +0600

    implement some string task

---
+++ index.js
@@ -0,0 +1,35 @@
+// Task-1:
+// Count how many times a string has the letter a
+
+// const letter =
+//   "I'm a job holder. I'm From Bangladesh. I want to be a web developer.";
+
+// const count = (letter.match(/a/g) || []).length;
+// console.log(count);
+
+// Task-2:
+// Count how many times a string has the letter a or A
+
+// const letter =
+//   "is an Emmet abbreviation used in code editors like Visual Studio Code to instantly generate 20 words of placeholder text. Typing this in HTML and pressing TAb (or Enter in some editors) produces A 20-word string for web design mockups. ";
+
+// const result = (letter.match(/a/gi) || []).length;
+// console.log(result);
+
+// Task-3:
+// Check whether a string contains all the vowels a, e, i, o, u
+
+const contain =
+  "is an Emmet abbreviation used in code editors like Visual Studio Code to instantly generate 20 words of placeholder text. Typing this in HTML and pressing Tab (or Enter in some editors) produces a 20-word string for web design mockups. ";
+
+if (
+  contain.includes("a") &&
+  contain.includes("e") &&
+  contain.includes("i") &&
+  contain.includes("o") &&
+  contain.includes("u")
+) {
+  console.log("All the character is matching");
+} else {
+  console.log("some not matched");
+}
